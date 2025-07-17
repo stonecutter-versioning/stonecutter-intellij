286adbac9f2937321c0be74eabe2a1929e294c8c
kikugie <stormikiku@gmail.com>
2025-07-17 22:43:29 +0200

    Introduce `StitcherDeclarationSearcher` for Kotlin POM target support and update plugin dependencies.

--- src/main/kotlin/dev/kikugie/stonecutter/intellij/editor/StitcherAnnotator.kt
+++ src/main/kotlin/dev/kikugie/stonecutter/intellij/editor/StitcherAnnotator.kt
@@ -3,19 +3,16 @@ package dev.kikugie.stonecutter.intellij.editor
 import com.intellij.lang.annotation.AnnotationHolder
 import com.intellij.lang.annotation.Annotator
 import com.intellij.lang.annotation.HighlightSeverity
-import com.intellij.openapi.editor.DefaultLanguageHighlighterColors
 import com.intellij.openapi.editor.colors.TextAttributesKey
 import com.intellij.psi.PsiElement
-import com.intellij.psi.util.elementType
 import dev.kikugie.stonecutter.intellij.editor.StitcherSyntaxHighlighter.AttributeKeys
-import dev.kikugie.stonecutter.intellij.lang.StitcherFile
-import dev.kikugie.stonecutter.intellij.lang.psi.*
+import dev.kikugie.stonecutter.intellij.lang.psi.StitcherConstant
+import dev.kikugie.stonecutter.intellij.lang.psi.StitcherDependency
+import dev.kikugie.stonecutter.intellij.lang.psi.StitcherReplacement
+import dev.kikugie.stonecutter.intellij.lang.psi.StitcherSwapId
 import dev.kikugie.stonecutter.intellij.model.SCProcessProperties
-import dev.kikugie.stonecutter.intellij.model.named
 import dev.kikugie.stonecutter.intellij.model.serialized.Replacement
 import dev.kikugie.stonecutter.intellij.service.stonecutterNode
-import dev.kikugie.stonecutter.intellij.service.stonecutterService
-import dev.kikugie.stonecutter.intellij.util.childrenSequence
 
 // TODO: move the missing value logic entirely to the inspection
 class StitcherAnnotator: Annotator {
