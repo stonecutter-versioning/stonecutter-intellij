4276ec1e35f16d55cf68a783501ae1f193c89a3a
kikugie <stormikiku@gmail.com>
2025-08-10 01:07:24 +0200

    Localize settings titles, labels, tooltips, and folding options strings into the resource bundle

--- src/main/kotlin/dev/kikugie/stonecutter/intellij/settings/StonecutterOptions.kt
+++ src/main/kotlin/dev/kikugie/stonecutter/intellij/settings/StonecutterOptions.kt
@@ -6,6 +6,8 @@ import com.intellij.openapi.options.ShowSettingsUtil
 import com.intellij.openapi.project.ProjectManager
 import com.intellij.openapi.ui.DialogPanel
 import com.intellij.ui.dsl.builder.panel
+import dev.kikugie.stonecutter.intellij.StonecutterBundle
+import dev.kikugie.stonecutter.intellij.StonecutterBundle.message
 import dev.kikugie.stonecutter.intellij.settings.StonecutterSettings.Companion.STATE
 import dev.kikugie.stonecutter.intellij.settings.config.PropertyConfigurable
 import dev.kikugie.stonecutter.intellij.settings.config.buttonSelector
@@ -16,20 +18,20 @@ import dev.kikugie.stonecutter.intellij.settings.variants.FoldingStyle
 
 class StonecutterOptions : BoundCompositeConfigurable<PropertyConfigurable>("Stonecutter Dev", null) {
     class FoldingOptions(title: String) : CodeFoldingOptionsProvider, PropertyConfigurable(title, {
-        buttonSelector("Mode", FoldingMode.entries, STATE::foldDisabledBlocks)
-        buttonSelector("Comment style", FoldingStyle.entries, STATE::foldedPresentation)
-        checkbox("Link regions", STATE::linkDisabledBlocks)
-            .tooltip("Fold Stonecutter regions together")
+        buttonSelector(message("stonecutter.settings.folding.mode"), FoldingMode.entries, STATE::foldDisabledBlocks)
+        buttonSelector(message("stonecutter.settings.folding.style"), FoldingStyle.entries, STATE::foldedPresentation)
+        checkbox(message("stonecutter.settings.folding.link"), STATE::linkDisabledBlocks)
     }) {
         @Suppress("unused") /* EP constructor */ constructor() : this("Stonecutter")
     }
 
-    class EditorOptions : PropertyConfigurable("Editor", {
-        checkbox("Use custom import optimiser", STATE::useImportOptimizer)
-        checkbox("Lock generated files", STATE::lockGeneratedFiles)
-        checkbox("Refresh project after switch", STATE::refreshAfterSwitch)
+    class EditorOptions : PropertyConfigurable(message("stonecutter.settings.editor"), {
+        checkbox(message("stonecutter.settings.editor.imports"), STATE::useImportOptimizer)
+            .tooltip(message("stonecutter.settings.editor.imports.tooltip"))
+        checkbox(message("stonecutter.settings.editor.lock"), STATE::lockGeneratedFiles)
+        checkbox(message("stonecutter.settings.editor.sync"), STATE::refreshAfterSwitch)
 
-        link("Configure Syntax Coloring") {
+        link(message("stonecutter.settings.editor.colours")) {
             ShowSettingsUtil.getInstance().showSettingsDialog(
                 ProjectManager.getInstance().defaultProject,
                 "Stonecutter"
@@ -39,7 +41,7 @@ class StonecutterOptions : BoundCompositeConfigurable<PropertyConfigurable>("Sto
 
     override fun createConfigurables(): List<PropertyConfigurable> = listOf(
         EditorOptions(),
-        FoldingOptions("Folding")
+        FoldingOptions(message("stonecutter.settings.folding"))
     )
 
     override fun createPanel(): DialogPanel = panel {
