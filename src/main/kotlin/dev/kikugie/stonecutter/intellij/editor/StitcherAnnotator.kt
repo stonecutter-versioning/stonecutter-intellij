209b66c19cb8e58e2fddc10008aa69f041d744f0
kikugie <stormikiku@gmail.com>
2025-08-11 11:52:00 +0200

    Deprecate `StitcherAnnotator` and remove unused reference annotation logic

--- src/main/kotlin/dev/kikugie/stonecutter/intellij/editor/StitcherAnnotator.kt
+++ src/main/kotlin/dev/kikugie/stonecutter/intellij/editor/StitcherAnnotator.kt
@@ -14,40 +14,8 @@ import dev.kikugie.stonecutter.intellij.model.SCProcessProperties
 import dev.kikugie.stonecutter.intellij.model.serialized.Replacement
 import dev.kikugie.stonecutter.intellij.service.stonecutterNode
 
-// TODO: move the missing value logic entirely to the inspection
+@Deprecated("Currently has no use")
 class StitcherAnnotator: Annotator {
-    private enum class ReferenceType(val attribute: TextAttributesKey, val variants: (SCProcessProperties) -> Collection<String>) {
-        CONSTANT(AttributeKeys.CONSTANT, { it.constants.keys }),
-        DEPENDENCY(AttributeKeys.DEPENDENCY, { it.dependencies.keys }),
-        REPLACEMENT(AttributeKeys.REPLACEMENT, { it.replacements.mapNotNull(Replacement::identifier) }),
-        SWAP(AttributeKeys.SWAP, { it.swaps.keys });
-    }
-
     override fun annotate(element: PsiElement, holder: AnnotationHolder) {
-        val reference = element.referenceType
-        if (reference != null) annotateReference(element, holder, reference)
-    }
-
-    private val PsiElement.referenceType get() = when (this) {
-        is StitcherConstant -> ReferenceType.CONSTANT
-        is StitcherDependency -> ReferenceType.DEPENDENCY
-        is StitcherReplacement -> ReferenceType.REPLACEMENT
-        is StitcherSwapId -> ReferenceType.SWAP
-        else -> null
-    }
-
-    private fun annotateReference(element: PsiElement, holder: AnnotationHolder, type: ReferenceType) {
-        holder.newSilentAnnotation(HighlightSeverity.INFORMATION)
-            .textAttributes(type.attribute)
-            .range(element)
-            .create()
-
-        val text = element.text
-        val properties = element.stonecutterNode?.params
-            ?: return
-        if (text !in type.variants(properties)) holder
-            .newAnnotation(HighlightSeverity.ERROR, "Invalid ${type.name.lowercase()} ID")
-            .range(element)
-            .create()
     }
 }
