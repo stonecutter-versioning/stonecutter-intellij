28b9a2d627a90cc930efedc4b6e64ea549517ca5
kikugie <stormikiku@gmail.com>
2025-09-29 00:26:27 +0200

    Refactor Gradle sync and task listeners for version compatibility
    Integrate conditional handling for `GradleSyncListener` vs. `GradleSyncContributor` and update `StonecutterTaskListener` methods to adapt to API changes for IntelliJ 2025+.

--- src/main/kotlin/dev/kikugie/stonecutter/intellij/service/gradle/GradleReloadListener.kt
+++ src/main/kotlin/dev/kikugie/stonecutter/intellij/service/gradle/GradleReloadListener.kt
@@ -1,18 +1,27 @@
+@file:Suppress("UnstableApiUsage")
+
 package dev.kikugie.stonecutter.intellij.service.gradle
 
 import com.intellij.ide.impl.ProjectUtil
 import com.intellij.ide.util.PropertiesComponent
+import com.intellij.platform.workspace.storage.MutableEntityStorage
 import dev.kikugie.stonecutter.intellij.service.stonecutterService
-import kotlinx.coroutines.runBlocking
 import org.jetbrains.plugins.gradle.model.ExternalProject
 import org.jetbrains.plugins.gradle.service.project.ProjectResolverContext
-import org.jetbrains.plugins.gradle.service.syncAction.GradleSyncListener
 import kotlin.io.path.Path
 
-@Suppress("UnstableApiUsage")
-object GradleReloadListener : GradleSyncListener {
-    override fun onModelFetchCompleted(context: ProjectResolverContext) = runBlocking {
-        val project = ProjectUtil.findProject(context.projectPath.let(::Path)) ?: return@runBlocking
+private typealias GradleSync =
+    /*? if <2025 {*/org.jetbrains.plugins.gradle.service.syncAction.GradleSyncContributor
+    /*?} else*//*org.jetbrains.plugins.gradle.service.syncAction.GradleSyncListener*/
+
+object GradleReloadListener : GradleSync {
+    //? if <2025 {
+    override suspend fun onModelFetchCompleted(context: ProjectResolverContext, storage: MutableEntityStorage) = updateStonecutterService(context)
+    //?} else
+    /*override fun onModelFetchCompleted(context: ProjectResolverContext) = kotlinx.coroutines.runBlocking { updateStonecutterService(context) }*/
+
+    private suspend fun updateStonecutterService(context: ProjectResolverContext) {
+        val project = ProjectUtil.findProject(context.projectPath.let(::Path)) ?: return
         val candidates = context.rootBuild.projects.mapNotNull { build ->
             context.getProjectModel(build, ExternalProject::class.java)
                 ?.takeIf { it.buildFile?.name?.startsWith("stonecutter.gradle") == true }
--- src/main/kotlin/dev/kikugie/stonecutter/intellij/service/gradle/StonecutterTaskListener.kt
+++ src/main/kotlin/dev/kikugie/stonecutter/intellij/service/gradle/StonecutterTaskListener.kt
@@ -1,6 +1,5 @@
 package dev.kikugie.stonecutter.intellij.service.gradle
 
-import com.intellij.execution.process.ProcessOutputType
 import com.intellij.openapi.externalSystem.autoimport.ExternalSystemProjectId
 import com.intellij.openapi.externalSystem.autoimport.ExternalSystemProjectTracker
 import com.intellij.openapi.externalSystem.model.task.ExternalSystemTaskId
@@ -11,13 +10,17 @@ import org.jetbrains.plugins.gradle.util.GradleConstants
 class StonecutterTaskListener : ExternalSystemTaskNotificationListener {
     private val switches: MutableSet<ExternalSystemTaskId> = mutableSetOf()
 
-    override fun onTaskOutput(id: ExternalSystemTaskId, text: String, outputType: ProcessOutputType) {
+    /*? if <2025 {*/override fun onTaskOutput(id: ExternalSystemTaskId, text: String, stdOut: Boolean)
+    /*?} else*//*override fun onTaskOutput(id: ExternalSystemTaskId, text: String, outputType: com.intellij.execution.process.ProcessOutputType)*/
+    {
         if (!StonecutterSettings.STATE.refreshAfterSwitch) return
         if (!text.startsWith("> Task :stonecutterSwitchTo")) return
         switches += id
     }
 
-    override fun onSuccess(projectPath: String, id: ExternalSystemTaskId) {
+    /*? if <2025 {*/override fun onSuccess(id: ExternalSystemTaskId)
+    /*?} else*//*override fun onSuccess(projectPath: String, id: ExternalSystemTaskId)*/
+    {
         if (id !in switches) return
         val project = id.findProject() ?: return
         val directory = project.basePath ?: return
@@ -27,7 +30,9 @@ class StonecutterTaskListener : ExternalSystemTaskNotificationListener {
         tracker.scheduleProjectRefresh()
     }
 
-    override fun onEnd(projectPath: String, id: ExternalSystemTaskId) {
+    /*? if <2025 {*/override fun onEnd(id: ExternalSystemTaskId)
+    /*?} else*//*override fun onEnd(projectPath: String, id: ExternalSystemTaskId)*/
+    {
         switches -= id
     }
 }
